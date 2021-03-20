d31c702be062b111072d1d1d820700d6420074f3
vadym <vasylaki95@gmail.com>
2021-03-20 20:43:35 +0200

    resolved bug wiht side navbar(on sepanding emplty space)

---
+++ src/app/globalComponents/sidebar-filler/sidebar-filler.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-filler',
+  templateUrl: './sidebar-filler.component.html',
+  styleUrls: ['./sidebar-filler.component.css']
+})
+export class SidebarFillerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
